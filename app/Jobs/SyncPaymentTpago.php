ac365fef63f5b1a965b992eb9031775a968c1e4a
Jose Ascurra <joseascurra123@gmail.com>
2023-06-09 08:34:09 -0400

    Cambios en la confirmacion de pago

--- app/Jobs/SyncPaymentTpago.php
+++ app/Jobs/SyncPaymentTpago.php
@@ -50,15 +50,17 @@ class SyncPaymentTpago implements ShouldQueue
                 foreach($collection->detail as $item){
                     for($i = 1; $i <= $item->quantity; $i++){
                         $userRaffle = UserRaffle::where('user_id',$collection->user_id)->where('raffle_id',$item->raffle_id)->first();
-                        $raffleNumbers = RaffleNumber::where('raffle_id',$item->raffle_id)->get(['number','client_id','created_at']);
+                        $raffleNumbers = RaffleNumber::where('raffle_id',$item->raffle_id)->get(['number','user_id','created_at']);
                         $number = 1;
                         if($raffleNumbers->count() > 0){
-                            $lastRaffleNumber = $raffleNumbers->where('client_id',$collection->client_id)->sortByDesc('created_at')->first();
+                            $lastRaffleNumber = $raffleNumbers->where('user_id',$collection->user_id)->sortByDesc('created_at')->first();
                             if(!empty($lastRaffleNumber)){
                                 $number = $lastRaffleNumber->number + 1;
                             }else if(!empty($userRaffle)){
                                 $number = !empty($userRaffle->min_number) ? $userRaffle->min_number : 1;
                             }
+                        }else if(!empty($userRaffle)){
+                            $number = !empty($userRaffle->min_number) ? $userRaffle->min_number : 1;
                         }
                         RaffleNumber::create([
                             'collection_id' => $item->collection_id,
