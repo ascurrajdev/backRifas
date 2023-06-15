a8df39588ddf03d3e90cf34bcb7456b31b7706fd
Jose Ascurra <joseascurra123@gmail.com>
2023-06-15 17:32:05 -0400

    Cambio en el test de login

--- tests/Feature/Api/LoginControllerTest.php
+++ tests/Feature/Api/LoginControllerTest.php
@@ -5,9 +5,11 @@ namespace Tests\Feature\Api;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
 
 class LoginControllerTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * @test
      */
@@ -25,5 +27,30 @@ class LoginControllerTest extends TestCase
             'email' => 'example@example.com'
         ]);
         $response->assertJsonValidationErrorFor("password");
+    }
+    /**
+     * @test
+     */
+    public function cannot_login_with_a_user_unexists(): void
+    {
+        $response = $this->postJson(route('api.login'),[
+            'email' => 'example@example.com',
+            'password' => 'password'
+        ]);
+        $response->assertJsonValidationErrorFor('email');
+    }
+    /**
+     * @test
+     */
+    public function can_login_with_a_user_exists(): void
+    {
+        User::factory()->create([
+            'email' => 'example@example.com'
+        ]);
+        $response = $this->postJson(route('api.login'),[
+            'email' => 'example@example.com',
+            'password' => 'password'
+        ]);
+        $response->assertSuccessful();
     }
 }
